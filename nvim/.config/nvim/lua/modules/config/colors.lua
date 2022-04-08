7d3b11e8d1fc277f784a8bb151498a1adcf1ce89
brikehn <thebriankwon@gmail.com>
2022-04-08 02:25:59 -0400

    just more junk

--- nvim/.config/nvim/lua/modules/config/colors.lua
+++ nvim/.config/nvim/lua/modules/config/colors.lua
@@ -1,14 +1,8 @@
 local g = vim.g
 
-g.tokyonight_style = "night" -- 'storm', 'night', or 'day' variant
-g.tokyonight_terminal_colors = true -- use terminal colors
-g.tokyonight_italic_comments = true -- italicize comments
-g.tokyonight_italic_keywords = true -- italicize keywords
-g.tokyonight_italic_functions = false -- italicize functions
-g.tokyonight_italic_variables = false -- italicize variables
-g.tokyonight_transparent = false -- transparent background
-g.tokyonight_sidebars = {} -- list of sidebar windows to for dark_sidebar
-g.tokyonight_dark_sidebar = true -- use darker sidebars
-g.tokyonight_dark_float = true -- use darker floats
-
+g.gruvbox_baby_function_style = "NONE"
+g.gruvbox_baby_keyword_style = "italic"
+g.gruvbox_baby_highlights = {Normal = {fg = "#123123", bg = "NONE", style="underline"}}
+g.gruvbox_baby_telescope_theme = 1
+g.gruvbox_baby_transparent_mode = 1
 return function() end
